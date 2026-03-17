4adae450b7aea67ab9400200d05041f7f7e01862
fikrahdamars <fikrahdamar7@gmail.com>
2026-03-18 00:28:50 +0700

    Initial Commit

---
+++ tasktapTests/tasktapTests.swift
@@ -0,0 +1,17 @@
+//
+//  tasktapTests.swift
+//  tasktapTests
+//
+//  Created by Fikrah Damar Huda on 18/03/26.
+//
+
+import Testing
+@testable import tasktap
+
+struct tasktapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
