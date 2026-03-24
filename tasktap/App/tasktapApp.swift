93e4f66aea7043740df317f2dd36ea4427fdc9e1
fikrahdamars <fikrahdamar7@gmail.com>
2026-03-24 18:44:32 +0700

    feat(model): adding task item model

--- tasktap/App/tasktapApp.swift
+++ tasktap/App/tasktapApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct tasktapApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            TaskItem.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct tasktapApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
         .modelContainer(sharedModelContainer)
     }
--- tasktap/Models/TaskItem.swift
+++ tasktap/Models/TaskItem.swift
@@ -7,3 +7,80 @@
 
 import Foundation
 import SwiftData
+
+enum Priority: Int, Codable, Equatable {
+    case low = 1
+    case medium = 2
+    case high = 3
+
+    var title: String {
+        switch self {
+        case .low: return "Low"
+        case .medium: return "Medium"
+        case .high: return "High"
+        }
+    }
+}
+
+enum RepeatFrequency: String, Codable, Equatable {
+    case none = "None"
+    case daily = "Daily"
+    case weekly = "Weekly"
+    case monthly = "Monthly"
+    case yearly = "Yearly"
+}
+
+@Model
+final class TaskItem: Equatable {
+    @Attribute(.unique) var id: UUID
+    var title: String
+    var taskDescription: String?
+    var priority: Priority
+    var deadline: Date
+    var notes: String?
+
+    var isCompleted: Bool
+    var createdAt: Date
+
+    var repeatFrequency: RepeatFrequency = RepeatFrequency.none
+    var repeatInterval: Int = 1
+    var repeatEndDate: Date?
+    var originalDeadline: Date
+
+    /// blm kutambahin ntaran
+    ///    var category: TaskCategory
+    @Attribute(.externalStorage) var attachment: Data?
+
+    init(title: String,
+         taskDescription: String? = nil,
+         priority: Priority = .low,
+         deadline: Date,
+         notes: String? = nil,
+         repeatFrequency: RepeatFrequency = .none,
+         repeatInterval: Int = 1,
+         repeatEndDate: Date? = nil,
+         attachment: Data? = nil,
+         isCompleted: Bool = false)
+    {
+        self.id = UUID()
+        self.title = title
+        self.taskDescription = taskDescription
+        self.priority = priority
+        self.deadline = deadline
+        self.notes = notes
+        
+        self.isCompleted = isCompleted
+        self.createdAt = .now
+        
+        self.originalDeadline = deadline
+        self.repeatFrequency = repeatFrequency
+        self.repeatInterval = repeatInterval
+        self.attachment = attachment
+        
+        if repeatFrequency != .none && repeatEndDate == nil {
+            self.repeatEndDate = Calendar.current.date(byAdding: .year, value: 1, to: deadline)
+        } else {
+            self.repeatEndDate = repeatEndDate
+        }
+    }
+}
