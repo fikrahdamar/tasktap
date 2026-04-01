c48faf0d43f02bbb324338c2938ed756ffae2efc
fikrahdamars <fikrahdamar7@gmail.com>
2026-04-01 22:22:56 +0700

    feat: setup custom font and fixing mainCard design

--- tasktap/UIComponents/Card/MainCard.swift
+++ tasktap/UIComponents/Card/MainCard.swift
@@ -10,35 +10,51 @@ import SwiftUI
 struct MainCard: View {
     var body: some View {
         GeometryReader { geo in
-            VStack (spacing: 30){
-                HStack {
-                    Text("DEEP WORK")
-                    Spacer()
-                    Image(systemName: "circle.circle")
-                }
-                .bold()
-                .foregroundStyle(Color.brown)
-                VStack (alignment: .leading){
-                    Text("Refactor Identity Module")
-                    Text("Clean up the core authentication logic for the next sprint.")
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                HStack {
-                    HStack{
-                        Image(systemName: "clock")
-                        Text("1h 30m").bold()
+            HStack {
+                RoundedRectangle(cornerRadius: 3)
+                    .fill(Color.brownAccent)
+                    .frame(width: 6)
+
+                VStack(spacing: 30) {
+                    
+                    HStack {
+                        Text("DEEP WORK")
+                        Spacer()
+                        Image(systemName: "circle.circle")
                     }
-                    HStack{
-                        Image(systemName: "exclamationmark")
-                        Text("High Priority")
-                    }
+                    .font(Font.custom("Inter-Medium", size: 12))
                     .bold()
-                    Spacer()
+                    .foregroundStyle(Color.brownAccent)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Refactor Identity Module")
+                            .font(Font.custom("Inter-Bold", size: 18))
+                        Text("Clean up the core authentication logic for the next sprint.")
+                            .font(Font.custom("Inter-Regular", size: 14))
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    HStack {
+                        HStack {
+                            Image(systemName: "clock")
+                            Text("1h 30m")
+                               
+                        }
+                        .font(Font.custom("Inter-bold", size: 12))
+                        HStack {
+                            Image(systemName: "exclamationmark")
+                            Text("High Priority")
+                        }
+                        .font(Font.custom("Inter-bold", size: 12))
+                        .bold()
+                        Spacer()
+                    }.foregroundStyle(Color.greyText)
                 }
-                
+                .padding(.horizontal, 25)
+                .padding(.vertical, 20)
+
             }
-            .padding(.horizontal,25)
-            .padding(.vertical, 20)
+            .fixedSize(horizontal: false, vertical: true)
             .frame(width: geo.size.width * 0.90)
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 15))
